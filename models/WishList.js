745304c9220a5953da8dfeac8a6f906b0b992f96
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-06-01 19:13:18 -0700

    add updates
    merged index.js to develop

--- models/WishList.js
+++ models/WishList.js
@@ -1,6 +1,6 @@
-// const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
-// const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection.js');
 
 class WishList extends Model { }
 
