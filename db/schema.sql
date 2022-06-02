2f5cc984737387ee0d47763d67155b5258e7d055
Alexander Lazar <alexanderjohnlazar@gmail.com>
2022-06-01 19:41:20 -0700

    Cleanup of files

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 DROP DATABASE IF EXISTS store_db;
 CREATE DATABASE store_db;
+=======
+DROP DATABASE IF EXISTS stores_db;
+
+USE DATABASE stores_db;
+>>>>>>> ad3ee77 (database and merge)
