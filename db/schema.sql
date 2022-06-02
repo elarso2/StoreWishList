73f0dd2475ea9da76bf82b2b084c8c1b4ee5fc42
Elizabeth Larson <elarso2@uw.edu>
2022-06-01 18:56:28 -0700

    adding npms, setting up ignore files, server.js file, and config connection

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
 DROP DATABASE IF EXISTS stores_db;
 
+<<<<<<< HEAD
 USE DATABASE stores_db;
+=======
+USE DATABASE stores_db;
+=======
+DROP DATABASE IF EXISTS store_db;
+CREATE DATABASE store_db;
+>>>>>>> 2ce9acb (adding npms, setting up ignore files, server.js file, and config connection)
+>>>>>>> 2d21573 (adding npms, setting up ignore files, server.js file, and config connection)
