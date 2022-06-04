6c451ab4b801e5dd832bf32a0e62bd6383863637
Alexander Lazar <alexanderjohnlazar@gmail.com>
2022-06-04 10:47:47 -0700

    Rebase and cleanup

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-DROP DATABASE IF EXISTS stores_db;
-
-<<<<<<< HEAD
-USE DATABASE stores_db;
-=======
-USE DATABASE stores_db;
-=======
-DROP DATABASE IF EXISTS store_db;
-CREATE DATABASE store_db;
->>>>>>> 2ce9acb (adding npms, setting up ignore files, server.js file, and config connection)
-<<<<<<< HEAD
->>>>>>> 2d21573 (adding npms, setting up ignore files, server.js file, and config connection)
-=======
-=======
-DROP DATABASE IF EXISTS store_db;
-CREATE DATABASE store_db;
-=======
 DROP DATABASE IF EXISTS stores_db;
 
 USE DATABASE stores_db;
->>>>>>> ad3ee77 (database and merge)
->>>>>>> 2f5cc98 (Cleanup of files)
->>>>>>> bc5a81d (Cleanup of files)
