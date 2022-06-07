2e637f2eb3ac3a8315fd72973fd3c591ca58c5bb
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-06-06 20:41:10 -0700

    add updates

--- utils/auth.js
+++ utils/auth.js
@@ -1,17 +1,9 @@
 const withAuth = (req, res, next) => {
-<<<<<<< HEAD
   if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
     next();
   }
-=======
-	if (!req.session.logged_in) {
-		res.redirect("/login");
-	} else {
-		next();
-	}
->>>>>>> 0c2fe9c (routes i made)
 };
 
 module.exports = withAuth;
