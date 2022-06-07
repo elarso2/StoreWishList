5775e5a52089dbcef8b15486e9a4eb24dbfbdbbb
Elizabeth Larson <elarso2@uw.edu>
2022-06-06 21:41:24 -0700

    fixing small errors with various files and npm packages
    revised files to resolve conflict issue

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./usersRoutes');
-const GiftCardRoutes= require('./giftcardsRoutes');
-const itemsroutes=require('./itemsRoutes');
-const storeRoutes =require('./storesRoutes');
+const GiftCardRoutes = require('./giftcardsRoutes');
+const itemsroutes = require('./itemsRoutes');
+const storeRoutes = require('./storesRoutes');
 
-
-router.use('/user',userRoutes);
-router.use('/giftCards',GiftCardRoutes);
+router.use('/user', userRoutes);
+router.use('/giftCards', GiftCardRoutes);
 router.use('/items', itemsroutes);
 router.use('/store', storeRoutes);
 
--- models/Items.js
+++ models/Items.js
@@ -2,36 +2,36 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Items extends Model { }
+class Items extends Model {}
 
 Items.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        item_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        store_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: "store",
-                key: "id",
-            },
-        },
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
+    item_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    store_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'store',
+        key: 'id',
+      },
+    },
+  },
 
-    {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "item",
-    }
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'item',
+  }
 );
 
 module.exports = Items;
