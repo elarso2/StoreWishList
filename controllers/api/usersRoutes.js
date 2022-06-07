21028e3b973cc74f05e95f4c0b6f76380d5aed48
Zane Pisano <pisanoz@live.com>
2022-06-06 21:37:09 -0700

    fixed err

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -21,7 +21,7 @@ router.post('/', async (req, res)=>{
 // login 
 router.post('/login', async (req, res)=>{
     try{
-        const userData =await User.findOne({where:{email: req.body.e}});
+        const userData =await User.findOne({where:{email: req.body.email}});
         if (!userData){
             res
             .status(400)
