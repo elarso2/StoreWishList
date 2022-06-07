597fb85198deef300b090fa6566ec53c9eed3f31
Zane Pisano <pisanoz@live.com>
2022-06-06 19:07:36 -0700

    added some routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -6,24 +6,24 @@ const { User,
 const withAuth = require('../utils/Auth')
 
 
-router.get('/', async (req , res)=>{
-    try{
-        const GiftCardData = await GiftCard.findAll(
-            {
-                exclude: [ value]
-            }
-        );
-        const cards = GiftCardData.map((GiftCard)=> GiftCard.get({plain: true}));
-        console.log(cards)
-        res.render("homepage",{
-            cards,
-            logged_in: req.session.logged_in,
-        });
+// router.get('/', async (req , res)=>{
+//     try{
+//         const GiftCardData = await GiftCard.findAll(
+//             {
+//                 exclude: [ value]
+//             }
+//         );
+//         const cards = GiftCardData.map((GiftCard)=> GiftCard.get({plain: true}));
+//         console.log(cards)
+//         res.render("homepage",{
+//             cards,
+//             logged_in: req.session.logged_in,
+//         });
 
-    }catch (err){
-        res.status(500).json(err);
-    }
-});
+//     }catch (err){
+//         res.status(500).json(err);
+//     }
+// });
 
 router.get('/login', (req,res)=>{
     if (req.session.logged_in){
