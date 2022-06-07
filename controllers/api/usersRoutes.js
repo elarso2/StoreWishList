b507eacaa80dc2356f507f5a5247bc86f76d626b
Elizabeth Larson <elarso2@uw.edu>
2022-06-06 21:41:24 -0700

    fixing small errors with various files and npm packages
    revised files to resolve conflict issue
    
    fixed merge conlficts

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -6,11 +6,17 @@ const withAuth = require('../../utils/auth');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
+<<<<<<< HEAD
     req.session.user_id = userData.id;
     req.session.logged_in = true;
     req.session.save(() => {
       // req.session.user_id = userData.id;
       // req.session.logged_in = true;
+=======
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+>>>>>>> 5775e5a (fixing small errors with various files and npm packages)
       res.status(200).json(userData);
     });
   } catch (err) {
