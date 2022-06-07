dbe5dfc642d8744e51335223c09ad3ef15d7b1dd
Zane Pisano <pisanoz@live.com>
2022-06-06 21:37:09 -0700

    fixed err
    fixed merge conflicts

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -15,22 +15,31 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
-// login
-router.post('/login', async (req, res) => {
-  try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
-    }
-    const validPassword = await userData.checkPassword(req.body.password);
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
-      return;
+// login 
+router.post('/login', async (req, res)=>{
+    try{
+        const userData =await User.findOne({where:{email: req.body.email}});
+        if (!userData){
+            res
+            .status(400)
+            .json({message:"Incorrect email or password, please try again" });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword){
+            res
+            .status(400)
+            .json({ message: "Incorrect email or password, please try again" });
+        return
+        }
+        req.session.save(()=>{
+            req.session.user_id=userData.id;
+            req.session.logged_in=true;
+            res.render('/profile', {User});
+            res.json({user : userData , message: "You are now logged in!" })
+        });
+    }catch (err){
+        res.status(400).json(err);
     }
     req.session.save(() => {
       req.session.user_id = userData.id;
