bbd840b659c698908247f5124412b06883a043a4
Zane Pisano <pisanoz@live.com>
2022-06-06 21:42:40 -0700

    fixed an err

--- controllers/api/storesRoutes.js
+++ controllers/api/storesRoutes.js
@@ -1,44 +1,39 @@
-const router =require ('express').Router();
+const router = require('express').Router();
 const req = require('express/lib/request');
 const res = require('express/lib/response');
-const { User,
-    Store,
-    GiftCard,
-    Items,}=require('../../models');
+const { User, Store, GiftCard, Items } = require('../../models');
 
-const withAuth = require('../../utils/auth')
-
+const withAuth = require('../../utils/auth');
 
 //create
-router.post('/', async (req,res)=>{
-    try{
-        const newStore = await Store.create({
-            ...req.body,
-            user_id: req.session.user_id,
-        });
-        res.status(200).json(newStore);
-    }catch (err){
-        res.status(400).json(err)
-    }
+router.post('/', async (req, res) => {
+  try {
+    const newStore = await Store.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newStore);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 //delete
-router.delete('/:id', async (req,res)=>{
-    try{
-        const storeData = await Store.destroy({
-            where:{
-                id: req.params.id,
-                user_id: req.session.user_id,
-            },
-        });
-        if (!storeData){
-            res.status(404).json({ message: 'No store found with this id!' });
+router.delete('/:id', async (req, res) => {
+  try {
+    const storeData = await Store.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!storeData) {
+      res.status(404).json({ message: 'No store found with this id!' });
       return;
-        }
-        res.status(200).json(storeData);
-    }catch(err){
-        res.status(500).json(err);
     }
+    res.status(200).json(storeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 //get store data
-
--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -1,59 +1,56 @@
-const router =require ('express').Router();
+const router = require('express').Router();
 
-const { User,
-    Store,
-    GiftCard,
-    Items,}=require('../../models')
-const withAuth = require('../../utils/auth')
+const { User, Store, GiftCard, Items } = require('../../models');
+const withAuth = require('../../utils/auth');
 // Create new user
-router.post('/', async (req, res)=>{
-    try{
-        const userData=await User.create(req.body);
-        req.session.save(()=>{
-            req.session.user_id = userData.id;
-            req.session.logged_in = true;
-            res.status(200).json(userData)
-        });
-    }catch (err){
-        res.status(400).json(err);
-    }
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
-// login 
-router.post('/login', async (req, res)=>{
-    try{
-        const userData =await User.findOne({where:{email: req.body.email}});
-        if (!userData){
-            res
-            .status(400)
-            .json({message:"Incorrect email or password, please try again" });
-            return;
-        }
-        const validPassword = await userData.checkPassword(req.body.password);
-        if (!validPassword){
-            res
-            .status(400)
-            .json({ message: "Incorrect email or password, please try again" });
-        return
-        }
-        req.session.save(()=>{
-            req.session.user_id=userData.id;
-            req.session.logged_in=true;
-            res.render('/profile', {User});
-            res.json({user : userData , message: "You are now logged in!" })
-        });
-    }catch (err){
-        res.status(400).json(err);
+// login
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
     }
+    const validPassword = await userData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      res.render('/profile', { User });
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 //logout
-router.post('/logout',(req, res)=>{
-    if (req.session.logged_in){
-        req.session.destroy(()=>{
-            res.status(204).end();
-        });
-    }else{
-        res.status(404).end();
-    }
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
 });
 
 module.exports = router;
