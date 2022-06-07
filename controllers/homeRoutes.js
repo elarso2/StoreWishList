c893404b1f9a6b818f2acf0b26033d86f7cdb9e1
Elizabeth Larson <elarso2@uw.edu>
2022-06-07 09:39:56 -0700

    router error fix, adjustments to hbs files to get site mildly functioning

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -21,9 +21,13 @@ const withAuth = require('../utils/auth');
 //     }
 // });
 
+router.get('/', (req, res) => {
+  res.render('../views/homepage');
+});
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect('../views');
     return;
   }
   res.render('login');
