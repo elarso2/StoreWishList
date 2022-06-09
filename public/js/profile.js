eb605bfd84ab80935cfd2f33929456360f089dca
Shang <shang@upgradestations.com>
2022-06-08 19:26:13 -0700

    changing username to name
    revised public/js files to resolve merge conflicts

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,23 +1,24 @@
 const newFormHandler = async (event) => {
-  event.preventDefault();
-
-  const username = document.querySelector('#username').value.trim();
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
-
-  if (username && email && password) {
-    const response = await fetch(`/api/user`, {
-      method: 'POST',
-      body: JSON.stringify({ username, email, password }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert('Failed to create an account please try again');
+    event.preventDefault();
+  
+    const name = document.querySelector('#name').value.trim();
+    const email = document.querySelector('#email').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (name && email && password) {
+      const response = await fetch(`/api/user`, {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/profile');
+      } else {
+        alert('Failed to create an account please try again');
+      }
     }
   }
 };
