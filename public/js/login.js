a1f83289916b6267e6eb109373a2e8e08b57e952
Shang <shang@upgradestations.com>
2022-06-08 21:03:50 -0700

    user signup fixed
    revised files to resolve merge conflicts

--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,24 @@
 const loginFormHandler = async (event) => {
-  event.preventDefault();
-
-  // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-
-  if (email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
     }
   };
   
@@ -29,9 +30,9 @@ const loginFormHandler = async (event) => {
     const password = document.querySelector('#password-signup').value.trim();
   
     if (name && email && password) {
-      const response = await fetch('/api/users', {
+      const response = await fetch('/api/user', {
         method: 'POST',
-        body: JSON.stringify({ name, email, password }),
+        body: JSON.stringify({ username: name, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
@@ -46,7 +47,8 @@ const loginFormHandler = async (event) => {
   document
     .querySelector('.login-form')
     .addEventListener('submit', loginFormHandler);
-  
+  console.log (document
+    .querySelector('.signup-form'))
   document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
