ea15364e7758ccc55506582951e5ea56cce4b119
Zane Pisano <pisanoz@live.com>
2022-06-08 21:05:26 -0700

    fixing routes

--- controllers/api/usersRoutes.js
+++ controllers/api/usersRoutes.js
@@ -35,7 +35,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      res.render('/profile', { User });
+      res.render('profile', userData);
       res.json({ user: userData, message: 'You are now logged in!' });
     });
   } catch (err) {
