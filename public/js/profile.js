ddf9654f1954d2eb223691013ccd881581b00e70
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-06-10 08:21:30 -0700

    revised syntax errors

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,24 +1,23 @@
 const newFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-  
-    if (name && email && password) {
-      const response = await fetch(`/api/user`, {
-        method: 'POST',
-        body: JSON.stringify({ username: name, email, password }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/profile');
-      } else {
-        alert('Failed to create an account please try again');
-      }
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch(`/api/user`, {
+      method: 'POST',
+      body: JSON.stringify({ username: name, email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to create an account please try again');
     }
   }
 };
